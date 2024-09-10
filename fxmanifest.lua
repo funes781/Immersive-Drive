29ce154ddd478e6c06d0699cf1b48266d057a326
funes781 <121256208+funes781@users.noreply.github.com>
2024-09-10 22:12:00 +0200

    Create fxmanifest.lua
    manifesting

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+--[[ FX Information ]]--
+fx_version   'cerulean'
+lua54        'yes'
+game         'gta5'
+
+--[[ Resource Information ]]--
+name         'fun-ImmersiveDrive'
+author       'funes781'
+version      '1.0.0'
+description  "fun-scripts"
+
+--[[ Manifest ]]--
+
+client_scripts { 
+    'client.lua',
+}
